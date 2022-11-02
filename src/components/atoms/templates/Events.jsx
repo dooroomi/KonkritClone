f865bab91ba30da09987de901e995c6a13edb67c
jiUmin <wjdwkjh94@gmail.com>
2022-11-02 16:11:44 +0900

    이벤트 타이틀 샤인 에니매이션 추가

--- src/components/atoms/templates/Events.jsx
+++ src/components/atoms/templates/Events.jsx
@@ -1,19 +1,24 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import * as colors from "@styles/colors";
 import SectionLayout from "@components/molecules/SectionLayout";
 import HideScrollX from "@components/molecules/HideScrollX";
-// const Container = styled.div`
-//   width: 100%;
-//   height: 398px;
-//   /* font-family: MarkPro-Heavy; //부모폰트 때문에 자식 폰트 덮혀서 주석*/
-//   font-size: 32px;
-//   padding: 40px 16px;
-// `;
 
 const EventsTopWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+`;
+
+//에니메이션 효과 만들기
+const shine = keyframes`
+  from{
+    background-position: 0%;
+    /* opacity: 0; */
+  }
+  to{
+    background-position: 200%;
+    /* opacity: 1; */
+  }
 `;
 
 const GradientSectionTitle = styled.span`
@@ -22,6 +27,11 @@ const GradientSectionTitle = styled.span`
   background: ${colors.textGradient};
 
   background-clip: text;
+
+  background-size: 200% auto;
+  background-position: 200%;
+  animation: ${shine} 4s linear infinite; //에니메이션: 효과 시간 속도 언제까지
+
   -webkit-background-clip: text;
   //배경색으로 글자 덮기
   -webkit-text-fill-color: transparent;
