e32d82d894dcd06f7ba0c934d67bac69fb50328a
jiUmin <wjdwkjh94@gmail.com>
2022-11-03 13:21:33 +0900

    useData 커스첨 훅스 제작

---
+++ src/hooks/useData.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useData(initialState, endpoint) {
+  const [data, setCollections] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      setIsError(false);
+      try {
+        const results = await axios(endpoint);
+        setCollections(results.data);
+        setIsLoading(false);
+      } catch {
+        setIsError(true);
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, [endpoint]);
+
+  return {data, isLoading, isError}; // {data:data, isLoading:isLoading, isError:isError}
+}
+
+export default useData;
